07660133035991213a296b284c3975aa09536018
MathNinja26 <Estherdentel@gmail.com>
2025-04-03 09:39:21 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-let choice = game.askForString("")
+let choice = ""
+let game2 = 0
+while (game2 == 0) {
+    choice = game.askForString("Would you like to play ")
+    if (choice == "yes") {
+    	
+    }
+}

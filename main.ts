6993788f7e16b737a5b7df4045e0c2274b89cae6
MathNinja26 <Estherdentel@gmail.com>
2025-04-03 11:19:36 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,27 @@ let game2 = 0
 while (game2 == 0) {
     choice = game.askForString("Do you want to play \"This or that? Yes or no?", 3)
     if (choice == "yes") {
+        game2 = 1
+    } else {
+        choice = game.askForString("Do you want to play \"Unscramble? Yes or no?", 3)
+        if (choice == "yes") {
+            game2 = 2
+        } else {
+            choice = game.askForString("Do you want to play \"Who did it? Yes or no?", 3)
+            if (choice == "yes") {
+                game2 = 3
+            } else {
+                game.splash("You must pick a game.")
+            }
+        }
+    }
+    if (game2 == 1) {
+    	
+    }
+    if (game2 == 2) {
+    	
+    }
+    if (game2 == 3) {
     	
     }
 }

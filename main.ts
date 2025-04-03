e55b40196af1818546cb9243251020dfdfdef9dc
MathNinja26 <Estherdentel@gmail.com>
2025-04-03 13:47:55 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,23 @@
 function whoDidIt () {
     tiles.setCurrentTilemap(tilemap`level1`)
+    game.setDialogFrame(img`
+        4 5 9 5 3 3 3 3 3 3 3 5 9 5 4 
+        5 9 5 b 6 1 e e e 1 6 b 5 9 5 
+        9 5 b 7 d 6 1 e 1 6 d 7 b 5 9 
+        5 b 7 2 d d 6 1 6 d d 2 7 b 5 
+        3 6 d d d d d 6 d d d d d 6 3 
+        3 1 6 d d d d d d d d d 6 1 3 
+        3 e 1 6 d d d d d d d 6 1 e 3 
+        3 e e 1 6 d d d d d 6 1 e e 3 
+        3 e 1 6 d d d d d d d 6 1 e 3 
+        3 1 6 d d d d d d d d d 6 1 3 
+        3 6 d d d d d 6 d d d d d 6 3 
+        5 b 7 2 d d 6 1 6 d d 2 7 b 5 
+        9 5 b 7 d 6 1 e 1 6 d 7 b 5 9 
+        5 9 5 b 6 1 e e e 1 6 b 5 9 5 
+        4 5 9 5 3 3 3 3 3 3 3 5 9 5 4 
+        `)
+    game.showLongText("You are an investagator. You have been hired to figure out who stole the cake. ", DialogLayout.Full)
 }
 let choice = ""
 let game2 = 0
